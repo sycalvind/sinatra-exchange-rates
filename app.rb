bff9fb8ced417940fb9e3f583609b3671718e888
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-02-02 18:41:45 +0000

    done

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require "dotenv/load"
+
+key = ENV.fetch("EXCHANGE_RATE_KEY")
+currencies_url = "https://api.exchangerate.host/list?access_key=#{key}"
+currencies_data = (HTTP.get(currencies_url)).to_s
+parsed_currencies = JSON.parse(currencies_data)
+ccy_list = parsed_currencies["currencies"].keys - ["BOB"]
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @currencies_list = ccy_list
+  erb(:home)
+end
+
+get("/:currency") do
+  @exchange_from = params.fetch("currency")
+  @currencies_list = ccy_list
+  erb(:convert_ccy)
+end
+
+get("/:from/:to") do
+  @from = params.fetch("from")
+  @to = params.fetch("to")
+  conversion_url = "https://api.exchangerate.host/convert?from=#{@from}&to=#{@to}&amount=1&access_key=#{key}"
+  conversion_data = HTTP.get(conversion_url).to_s
+  parsed_conversion = JSON.parse(conversion_data)
+  @result = parsed_conversion["result"]
+  erb(:converted)
 end
